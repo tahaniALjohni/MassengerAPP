36da5cdd66df17b7c99d3b7d40d9c0d805f9f628
tahaniALjohni <90762535+tahaniALjohni@users.noreply.github.com>
2021-10-27 14:32:23 +0300

    section1

---
+++ massengerApp/Controllers/LogReg/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  massengerApp
+//
+//  Created by T Aljohni on 21/03/1443 AH.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ massengerApp/Resources/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  massengerApp
+//
+//  Created by T Aljohni on 21/03/1443 AH.
+//
+
+import Foundation
